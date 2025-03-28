cfe7f510f9a4b56e172a26be69e39d2f9b5fb864
rebecabg <rebecabrgarcia@gmail.com>
2025-03-28 15:05:36 -0300

    configura o inicio do projeto

---
+++ app/routes.py
@@ -0,0 +1,47 @@
+from app import app, db
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm #, RegistrationForm
+from urllib.parse import urlsplit
+from flask_login import login_user, logout_user, current_user, login_required
+import sqlalchemy as sa
+from app.models import User
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    user={'username':'Wanderson'}
+    posts=[
+        {
+            'author':{'username': 'Joao'},
+            'body': 'Belo dia em Vila Velha!'
+        },
+        {
+           'author': {'username': 'Maria'},
+           'body': 'Bora para o cinema hoje?'
+        },
+    ]
+    return render_template('index.html', title='Home', posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return  redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)) 
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return  redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title = 'SignIn', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
--- teste.py
+++ teste.py
@@ -1,1 +1,24 @@
-# arquivo teste
+import hashlib
+import secrets
+
+# mesg1 = "ola mundo!"
+
+# hash_md5 = hashlib.md5(mesg1.encode())
+# print(hash_md5.hexdigest())
+
+# hash_sha1 = hashlib.sha1(mesg1.encode())
+# print(hash_sha1.hexdigest())
+
+# hash_sha256 = hashlib.sha256(mesg1.encode())
+# print(hash_sha256.hexdigest())
+
+# hash_sha512 = hashlib.sha512(mesg1.encode())
+# print(hash_sha512.hexdigest())
+
+salt = secrets.token_hex(16)
+print(salt)
+password="abb"
+
+hash_pass = hashlib.sha256((password + salt).encode())
+print(hash_pass.hexdigest())
+
