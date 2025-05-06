425543655f6acd014e5d4bd5bb7b940a2f974f56
rebecabg <rebecabrgarcia@gmail.com>
2025-05-06 07:51:34 -0300

    cria conexao com banco remoto e cria pyproject

--- config.py
+++ config.py
@@ -1,6 +1,22 @@
 import os
+from dotenv import load_dotenv
+from sqlalchemy.pool import NullPool
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()  # carrega o .env
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'voce-nunca-saberah'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}?sslmode=require"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'poolclass': NullPool
+    }
